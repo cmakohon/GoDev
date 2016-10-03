65fe7ce6711c6834cc97609310c72416b9d81ac9
cmakohon <cmakohon@me.com>
2016-10-02 23:57:11 -0400

    Create main.php

---
+++ main.php
@@ -0,0 +1,130 @@
+<?php
+define( 'YOURLS_ADMIN', true );
+require_once( dirname( dirname( __FILE__ ) ).'/includes/load-yourls.php' );
+
+yourls_maybe_require_auth(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>go.unc.edu</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+
+    <!-- Google font link -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400" rel="stylesheet">
+
+    <script type="text/javascript">
+        //<![CDATA[
+        var ajaxurl  = '<?php echo yourls_admin_url( 'admin-ajax.php' ); ?>';
+        var zclipurl = '<?php yourls_site_url(); ?>/js/ZeroClipboard.swf';
+        //]]>
+    </script>
+
+    <script src="<?php yourls_site_url(); ?>/js/jquery-1.9.1.min.js?v=<?php echo YOURLS_VERSION; ?>" type="text/javascript"></script>
+    <script src="<?php yourls_site_url(); ?>/js/common.js?v=<?php echo YOURLS_VERSION; ?>" type="text/javascript"></script>
+    <script src="<?php yourls_site_url(); ?>/js/insert.js?v=<?php echo YOURLS_VERSION; ?>" type="text/javascript"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script>
+        var ajaxurl  = '<?php echo yourls_admin_url( 'user-ajax.php' ); ?>';
+    </script>
+
+    <![endif]-->
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a href="http://www.unc.edu/" class="navbar-left"><img src="img/small_blue_unc.png" id="unclogo" height="50"></a>
+            <!--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">-->
+                <!--<span class="sr-only">Toggle navigation</span>-->
+                <!--<span class="icon-bar"></span>-->
+                <!--<span class="icon-bar"></span>-->
+                <!--<span class="icon-bar"></span>-->
+            <!--</button>-->
+            <!--<a class="navbar-brand" href="#">go.unc.edu</a>-->
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <form class="navbar-form navbar-right">
+                <button type="submit" class="btn btn-primary">onyen login</button>
+            </form>
+        </div><!--/.navbar-collapse -->
+    </div>
+</nav>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container">
+        <h1 style="font-size: 52px; font-weight: 200;">go.unc.edu</h1>
+        <p>A UNC hosted URL shortener.</p>
+        <!--<label for="basic-url">Your vanity URL</label>-->
+        <div class="form-group form-group-lg">
+            <input type="text" class="form-control" id="add-url" placeholder="Your URL" aria-describedby="basic-addon3">
+        </div>
+        <?php yourls_nonce_field( 'add_url', 'nonce-add' ); ?>
+
+        <div class="input-group input-group-lg">
+            <span class="input-group-addon" id="basic-addon3">https://go.unc.edu/</span>
+            <input type="text" class="form-control" id="add-keyword" placeholder="Custom Short URL" aria-describedby="basic-addon3">
+            <span class = "input-group-btn">
+                  <button class = "btn btn-default" type = "button">
+                     Random
+                  </button>
+               </span>
+        </div>
+        <br>
+        <div class="col-md-12 text-center">
+            <p><a class="btn btn-success btn-lg" href="#" role="button"  onclick="add_link();">Submit &raquo;</a></p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-md-6">
+            <h2>What is go.unc.edu?</h2>
+            <p>go.unc.edu is a UNC hosted URL shortener. If you have an active Onyen, you can enter your long URL (such as a Google Doc) and the tool will give you a shortened address (custom or random) prefixed by go.unc.edu. </p>
+<!--            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>-->
+        </div>
+        <div class="col-md-6">
+            <h2>Need help?</h2>
+            <p>Click below to view our step-by-step tutorial.</p>
+            <p><a class="btn btn-default" href="http://godev.web.unc.edu/user-manual/" target="_blank" role="button">Go &raquo;</a></p>
+        </div>
+    </div>
+
+    <hr>
+
+    <footer>
+        <p>&copy; 2016 The University of North Carolina at Chapel Hill.</p>
+    </footer>
+</div> <!-- /container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
